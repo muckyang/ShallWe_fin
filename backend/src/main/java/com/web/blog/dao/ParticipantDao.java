4954a745d24b999bec5c925c2b84692cbb874ad3
김영남 <yn782@naver.com>
2020-08-04 15:17:58 +0900

    postController 수정

--- backend/src/main/java/com/web/blog/dao/ParticipantDao.java
+++ backend/src/main/java/com/web/blog/dao/ParticipantDao.java
@@ -1,15 +1,20 @@
 package com.web.blog.dao;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.web.blog.model.participant.Participant;
 import com.web.blog.model.post.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface ParticipantDao extends JpaRepository<Participant, String> {
-    
+
     Post getPostByArticleId(int articleId);
+
     Participant getParticipantByNo(int no);
+
+    Optional<Participant> getParticipantByUserIdAndArticleId(int userId, int articleId);
+
     List<Participant> findParticipantByArticleId(int articleId);
 }
