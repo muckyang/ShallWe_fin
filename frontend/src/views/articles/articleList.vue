a957c18fe784e83268cee0662f002dfe17016fe7
차종욱 <hedge0207@gmail.com>
2020-08-06 19:46:57 +0900

    likenum, commentnum

--- frontend/src/views/articles/articleList.vue
+++ frontend/src/views/articles/articleList.vue
@@ -39,8 +39,8 @@
                     <template v-slot:footer>
                       <div class="d-flex justify-content-between">
                           <small>
-                            <b-icon-heart></b-icon-heart> 0개
-                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> 0개
+                            <b-icon-heart></b-icon-heart> {{article.likeNum}}개
+                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> {{article.commentNum}}
                           </small>
                           <small class="text-muted">{{ article.timeAgo }}</small>
                       </div>
@@ -69,8 +69,8 @@
                     <template v-slot:footer>
                       <div class="d-flex justify-content-between">
                           <small>
-                            <b-icon-heart></b-icon-heart> 0개
-                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> 0개
+                            <b-icon-heart></b-icon-heart> {{article.likeNum}}개
+                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> {{article.commentNum}}
                           </small>
                           <small class="text-muted">{{ article.timeAgo }}</small>
                       </div>
@@ -100,8 +100,8 @@
                     <template v-slot:footer>
                       <div class="d-flex justify-content-between">
                           <small>
-                            <b-icon-heart></b-icon-heart> 0개
-                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> 0개
+                            <b-icon-heart></b-icon-heart> {{article.likeNum}}개
+                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> {{article.commentNum}}
                           </small>
                           <small class="text-muted">{{ article.timeAgo }}</small>
                       </div>
