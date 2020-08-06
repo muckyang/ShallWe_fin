87bb29faad0f381aadf333e6d085faf337512d10
henryhansollee <henryhansollee@gmail.com>
2020-08-06 23:31:02 +0900

    Update searchList.vue Layout

--- frontend/src/views/searchList.vue
+++ frontend/src/views/searchList.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <div class="container">
-      <div class="row">
-        <div class="card col-3" style="width: 18rem;" v-for="article in articles" :key="article.articleId">
-          <img src="https://source.unsplash.com/random" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">{{ article.title }}</h5>
-            <p class="card-text">{{ article.description }}</p>
-            <router-link class="btn btn-primary" :to="{name:'articleDetail',params:{ID:`${article.articleId}`}}">보러가기</router-link>
-          </div>
-        </div>
-      </div>
-    </div>
+     <b-container class="bv-example-row">
+          <b-row align-h="start">
+            <b-col cols="4"
+            v-for="article in articles" 
+            :key="article.articleId">
+                <router-link :to="{name:'articleDetail',params:{ID:`${article.articleId}`}}" class="text-decoration-none text-dark">
+                  <b-card class="article-card m-4 _card card__one" align="left" img-width="100%" 
+                img-height="60%" :img-src="article.image"
+                img-alt="Image" img-top>
+                    <b-card-text>
+                      <h5 class="article-title">{{ article.title }}</h5>
+                      <h6 class="article-area">{{ article.address }}</h6>
+                      <h6 class="article-price">가격: {{ article.sumPrice }}원/{{ article.minPrice }}원</h6>
+                    </b-card-text>
+                    <template v-slot:footer>
+                      <div class="d-flex justify-content-between">
+                          <small>
+                            <b-icon-heart></b-icon-heart> {{article.likeNum}}개
+                            <b-icon-chat-dots class="ml-1"></b-icon-chat-dots> {{article.commentNum}}개
+                          </small>
+                          <small class="text-muted">{{ article.timeAgo }}</small>
+                      </div>
+                    </template>
+                </b-card>
+              </router-link>
+            </b-col>
+          </b-row>
+        </b-container>        
   </div>
 </template>
 
@@ -31,6 +47,50 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+$color-bg: #f8f8f8;
+$card-padding: 20px;
+$grid-gutter: 31px;
 
+// Media Queries breakpoints
+$small: 480px;
+$medium: 768px;
+$large: 992px;
+$x-large: 1200px;
+
+._card {
+  position: relative;
+  flex: 1 1 100%;
+  background: lighten($color-bg, 3%);
+
+  @media screen and (min-width: $medium) {
+    flex-basis: calc(33.33% - (#{$grid-gutter * 2} + #{$card-padding * 2}));
+    margin: 0 $grid-gutter;
+  }
+}
+.card__one {
+  transition: transform .5s;
+
+  &::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 2s cubic-bezier(.165, .84, .44, 1);
+    box-shadow: 0 8px 17px 0 rgba(0, 0, 0, .2), 0 6px 20px 0 rgba(0, 0, 0, .15);
+    content: '';
+    opacity: 0;
+    z-index: -1;
+  }
+
+  &:hover,
+  &:focus {
+    transform: scale3d(1.006, 1.006, 1);
+
+    &::after {
+      opacity: 1;
+    }
+  }
+}
 </style>
