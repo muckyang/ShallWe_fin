90740c8cb68dd581fed5bd31c66541447755edc3
henryhansollee <henryhansollee@gmail.com>
2020-08-09 03:19:51 +0900

    Make adminPage.vue template

--- frontend/src/components/user/adminPage.vue
+++ frontend/src/components/user/adminPage.vue
@@ -1,5 +1,43 @@
 <template>
-  
+  <div>
+    <h1>관리자 페이지</h1>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">이메일</th>
+          <th scope="col">이름</th>
+          <th scope="col">닉네임</th>
+          <th scope="col">지역</th>
+          <th scope="col">회원관리</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>이메일</td>
+          <td>이름</td>
+          <td>닉네임</td>
+          <td>지역</td>
+          <td>
+            <button class="btn btn-warning btn-sm">신고내역</button>
+            <button class="btn btn-danger btn-sm">회원삭제</button>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td>이메일</td>
+          <td>이름</td>
+          <td>닉네임</td>
+          <td>지역</td>
+          <td>
+            <button class="btn btn-warning btn-sm">신고내역</button>
+            <button class="btn btn-danger btn-sm">회원삭제</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
