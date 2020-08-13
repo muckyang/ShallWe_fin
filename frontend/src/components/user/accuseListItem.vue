311a52df1b5c17ace83c6a9ca84e6ccb6ffb3f5c
henryhansollee <henryhansollee@gmail.com>
2020-08-13 20:10:41 +0900

    accuseListItem.vue - 반영

---
+++ frontend/src/components/user/accuseListItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <tr>
+      <th scope="row">{{ accuse.accuseId }}</th>
+      <td>{{ accuse.reporter }}</td>
+      <td>{{ accuse.defendant }}</td>
+      <td>{{ accuse.accuseIndex }}</td>
+      <td>{{ accuse.accuseKind }}</td>
+      <td>{{ accuse.accuseReason }}</td>
+      <td>
+        <button class="btn btn-info btn-sm"><a :href="accuse.accuseUrl" class="text-decoration-none text-white">신고상세</a></button>
+        <button class="btn btn-danger btn-sm" @click="yesAccuse()">신고접수</button>
+        <button class="btn btn-secondary btn-sm" @click="noAccuse()">허위신고</button>
+      </td>
+    </tr>
+  </div>
+</template>
+
+<script>
+const BACK_URL = process.env.VUE_APP_BACK_URL;
+import axios from "axios";
+import cookies from "vue-cookies";
+
+export default {
+  name: "accuseListItem",
+  props: {
+    accuse: Object,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    decideAccuse() {
+      this.accuse.token=this.$cookies.get('admin-token')
+      console.log(this.accuse)
+      axios
+        .post(`${BACK_URL}/accuse/applyto`, this.accuse)
+        .then(() => {
+          console.log(this.accuse, 'AAAAAAAAAAAAAAA')
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    yesAccuse() {
+      this.accuse.accuseConfirm = 1
+      this.decideAccuse();
+      console.log(this.accuse, 'BBBBB')
+    },
+    noAccuse() {
+      this.accuse.accuseConfirm = 1
+      this.decideAccuse();
+      console.log(this.accuse, 'BBBBB') 
+    },
+  }
+};
+</script>
+
+<style>
+
+</style>
