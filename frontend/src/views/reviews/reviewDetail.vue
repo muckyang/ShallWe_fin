4af1382503a2796a3921b6b7f29ac3ed78a8781a
임기람 <rlfka@naver.com>
2020-08-15 13:16:50 +0900

    디자인 손보는중

--- frontend/src/views/reviews/reviewDetail.vue
+++ frontend/src/views/reviews/reviewDetail.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="container">
     <div class="review-container">
+      <div class="review-drop dropdown dropleft" v-if="review.userId === userData.userId">
+        <button type="button" class="review-btn" data-toggle="dropdown">
+          <i class="fas fa-ellipsis-h"></i>
+        </button>
+        <div class="dropdown-menu">
+          <router-link
+            class="reviewUpdate"
+            :to="{
+                      name: 'reviewUpdate',
+                      params: { ID: this.review.articleId},
+                    }"
+          >
+            <a class="dropdown-item">수정</a>
+          </router-link>
+          <a class="dropdown-item">삭제</a>
+        </div>
+      </div>
       <div class="review-top">
         <div class="review-info">
           <div class="review-title">{{review.title}}</div>
           <div class="review-writer">{{review.writer}}</div>
           <div class="review-time">{{review.timeAgo}}</div>
-          <div class="comment-drop dropdown dropleft" v-if="review.userId === userData.userId">
-            <button type="button" class="comment-btn" data-toggle="dropdown">
-              <i class="fas fa-ellipsis-v"></i>
-            </button>
-            <div class="dropdown-menu">
-              <router-link
-                class="reviewUpdate"
-                :to="{
-                    name: 'reviewUpdate',
-                    params: { ID: this.review.articleId},
-                  }"
-              >
-                <a class="dropdown-item">수정</a>
-              </router-link>
-              <a class="dropdown-item">삭제</a>
-            </div>
-          </div>
         </div>
         <div class="review-like-comment">
-          <div class="review-like">
+          <div class="like-container">
             <reviewLike @like-change="likeChange" :isLiked="isLiked" :review="review" />
             {{review.likeNum}}
           </div>
           <div class="review-comment">
-            <i class="far fa-comment-dots"></i>
+            <div class="comment-icon">
+              <i class="far fa-comment-dots"></i>
+            </div>
             {{review.commentNum}}
           </div>
         </div>
@@ -116,10 +118,15 @@ export default {
   display: flex;
   flex-direction: column;
 }
-.review-time {
-  /* display: flex; */
-  /* justify-content: flex-end; */
-  /* margin: 10px 8px 0 8px; */
+.review-drop {
+  display: flex;
+  justify-content: flex-end;
+}
+.review-btn {
+  border: none;
+  outline: none;
+  background-color: transparent;
+  color: rgb(197, 192, 192);
 }
 .review-info {
   display: flex;
@@ -129,6 +136,9 @@ export default {
 .review-title {
   font-weight: bold;
   font-size: 20px;
+}
+.review-time {
+  opacity: 0.7;
 }
 .review-title,
 .review-writer,
@@ -142,11 +152,17 @@ export default {
 .review-like-comment {
   display: flex;
   justify-content: flex-end;
+  vertical-align: middle;
 }
-.review-like,
+.like-container,
 .review-comment {
   display: flex;
-  margin: 0 8px;
+}
+.like-container {
+  margin: 0 5px;
+}
+.comment-icon {
+  margin: 0 3px;
 }
 .review-bottom {
   border: 1px solid red;
