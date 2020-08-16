a1669a08945302672c98c9c3411b1f78c15be08e
henryhansollee <henryhansollee@gmail.com>
2020-08-16 23:20:33 +0900

    accuseListItem.vue - Layout

--- frontend/src/components/user/accuseListItem.vue
+++ frontend/src/components/user/accuseListItem.vue
@@ -1,33 +1,40 @@
 <template>
   <div>
-    <div>
-      <b-card>
-        <b-card-title># {{ accuse.accuseId }}. "{{ accuse.reporter }}" 님의 신고 내용</b-card-title>
-        <b-card-sub-title>피신고자: {{ accuse.defendant }}</b-card-sub-title>
-
-        <b-card-text v-if="accuse.accuseIndex === 1">
-          <span class="badge badge-pill badge-light mr-2">게시물 신고</span>
-          <span v-if="accuse.accuseKind == 1">욕설</span>
-          <span v-if="accuse.accuseKind == 2">노쇼</span>
-          <span v-if="accuse.accuseKind == 3">광고</span>
-        </b-card-text>
-        <b-card-text v-if="accuse.accuseIndex === 2">
-          <span class="badge badge-pill badge-dark mr-2">댓글 신고</span>
-          <span v-if="accuse.accuseKind == 1">욕설</span>
-          <span v-if="accuse.accuseKind == 2">노쇼</span>
-          <span v-if="accuse.accuseKind == 3">광고</span>
-        </b-card-text>
-
-        <b-card-text
-          style="border: solid 0.5px gray; width: 70%; margin: auto;"
-        >{{ accuse.accuseReason }}</b-card-text>
-        <button class="btn btn-info btn-sm mt-3">
-          <a :href="accuse.accuseUrl" class="text-decoration-none text-white">신고상세</a>
-        </button>
-        <button class="btn btn-danger btn-sm mt-3" @click="yesAccuse()">신고접수</button>
-        <button class="btn btn-secondary btn-sm mt-3" @click="noAccuse()">허위신고</button>
-      </b-card>
-    </div>
+    <b-media tag="li" class="text-left">
+      <template v-slot:aside>
+        <b-img blank blank-color="#abc" width="64" alt="placeholder"></b-img>
+      </template>
+      <div class="d-flex justify-content-between">
+        <h5 class="mt-0 mb-1"># {{ accuse.accuseId }}. "{{ accuse.reporter }}" 님의 신고 내용</h5>
+        <div>
+          <div class="article-drop dropdown dropleft">
+            <button type="button" class="article-btn" data-toggle="dropdown">
+              <i class="fas fa-ellipsis-v"></i>
+            </button>
+            <div class="dropdown-menu">
+              <a :href="accuse.accuseUrl" class="dropdown-item">신고상세</a>
+              <a :href="accuse.accuseUrl" @click="yesAccuse()" class="dropdown-item">신고접수</a>
+              <a :href="accuse.accuseUrl" @click="noAccuse()" class="dropdown-item">허위신고</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h6>피신고자: {{ accuse.defendant }}</h6>
+      <h6 v-if="accuse.accuseIndex === 1">
+        <span class="badge badge-pill badge-light mr-2">게시물 신고</span>
+        <span v-if="accuse.accuseKind == 1">욕설</span>
+        <span v-if="accuse.accuseKind == 2">노쇼</span>
+        <span v-if="accuse.accuseKind == 3">광고</span>
+      </h6>
+      <h6 v-if="accuse.accuseIndex === 2">
+        <span class="badge badge-pill badge-dark mr-2">댓글 신고</span>
+        <span v-if="accuse.accuseKind == 1">욕설</span>
+        <span v-if="accuse.accuseKind == 2">노쇼</span>
+        <span v-if="accuse.accuseKind == 3">광고</span>
+      </h6>
+      <p class="mb-0">{{ accuse.accuseReason }}</p>
+    </b-media>
+    <hr>
   </div>
 </template>
 
