d731ddb931a7ed0f8ec4dcf9abb333d193edb2ed
김영남 <yn782@naver.com>
2020-08-18 15:34:25 +0900

    joinlist 수정

--- backend/src/main/java/com/web/blog/controller/AccountController.java
+++ backend/src/main/java/com/web/blog/controller/AccountController.java
@@ -187,22 +187,23 @@ public class AccountController {
                 result.likeList.add(postDao.findPostByArticleId(llist.get(i).getArticleId()));
             }
 
-            result.joinList = new LinkedList<>();
+    
+          
+
             result.completeList = new LinkedList<>();
             List<Participant> partlist = partDao.getParticipantByUserIdAndStatus(userId,1);// 참가 수락된 것 중 
             for(int i = 0 ; i < partlist.size();i++){
                 result.completeList.add(postDao.getPostByArticleIdAndStatus(partlist.get(i).getArticleId(),4));
             }
 
-
-            result.completeList = new LinkedList<>();
+            result.joinList = new LinkedList<>();
             List<Integer> numlist = new LinkedList<>();
             numlist.add(1);
             numlist.add(2);
             numlist.add(4);
             List<Participant> partlist2 = partDao.getParticipantByUserIdAndStatus(userId,1);// 참가 수락된 것 중 
             for(int i = 0 ; i < partlist2.size();i++){
-                result.completeList.add(postDao.getPostByArticleIdAndStatusIn(partlist2.get(i).getArticleId(),numlist));
+                result.joinList.add(postDao.getPostByArticleIdAndStatusIn(partlist2.get(i).getArticleId(),numlist));
             }
 
 
