afe586c5f039afaca4eded4fcc97b5cc9cc29183
henryhansollee <henryhansollee@gmail.com>
2020-08-18 13:55:44 +0900

    마이페이지 목록 추가

--- frontend/src/components/user/Profile.vue
+++ frontend/src/components/user/Profile.vue
@@ -1,6 +1,5 @@
 <template>
 <div class="mt-5">
-  <!-- 1. 상단 -->
   <div class="mb-4 container">
     <div class="row">
       <div class="col-6">
@@ -42,79 +41,62 @@
       </div>
     </div>
 
-      <!-- 2. 중단 -->
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <b-card bg-variant="light" text-variant="dark" class="mb-4">
-              <b-card-text>
-                <div class="container">
-                  <div class="row">
-                    <div class="col-3">
-                      <h2>{{ userData.articleCount }} 개</h2>
-                      <small>내가 쓴 게시물</small>
-                    </div>
-                    <div class="col-3">
-                      <h2>{{ userData.reviewCount }} 개</h2>
-                      <small>내가 쓴 후기</small>
-                    </div>
-                    <div class="col-3">
-                      <h2>{{ userData.likeCount }} 개</h2>
-                      <small>찜 목록</small>
-                    </div>
-                    <div class="col-3">
-                      <h2>{{ userData.tempCount }} 개</h2>
-                      <small>임시 저장</small>
-                    </div>
-                  </div>
-                </div>
-              </b-card-text>
-            </b-card>
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <div class="list-group flex-column" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <a class="list-group-item list-group-item-action" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">내가 쓴 게시물</a>
+            <a class="list-group-item list-group-item-action" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">내가 쓴 후기</a>
+            <a class="list-group-item list-group-item-action" id="v-pills-3-tab" data-toggle="pill" href="#v-pills-3" role="tab" aria-controls="v-pills-3" aria-selected="true">참여한 게시물</a>
+            <a class="list-group-item list-group-item-action" id="v-pills-4-tab" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-4" aria-selected="false">거래 완료 게시물</a>
+            <a class="list-group-item list-group-item-action" id="v-pills-5-tab" data-toggle="pill" href="#v-pills-5" role="tab" aria-controls="v-pills-5" aria-selected="false">찜 목록</a>
+            <a class="list-group-item list-group-item-action" id="v-pills-6-tab" data-toggle="pill" href="#v-pills-6" role="tab" aria-controls="v-pills-6" aria-selected="false">임시 저장</a>
+          </div>
+        </div>
+        <div class="col-9">
+          <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active text-dark" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">
+              내가 쓴 게시물 목록
+              {{ userData.articleList }}
+            </div>
+            <div class="tab-pane fade text-dark" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-2-tab">
+              내가 쓴 후기 목록
+              {{ userData.reviewList }}
+            </div>
+            <div class="tab-pane fade text-dark" id="v-pills-3" role="tabpanel" aria-labelledby="v-pills-3-tab">
+              참여한 게시물
+              {{ userData }}
+            </div>
+            <div class="tab-pane fade text-dark" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-4-tab">
+              거래 완료 게시물
+              {{ userData }}
+            </div>
+            <div class="tab-pane fade text-dark" id="v-pills-5" role="tabpanel" aria-labelledby="v-pills-5-tab">
+              내가 찜한 목록
+              {{ userData.likeList }}
+            </div>
+            <div class="tab-pane fade text-dark" id="v-pills-6" role="tabpanel" aria-labelledby="v-pills-6-tab">
+              <div v-for="article in articles" :key="article.articleId">
+                <router-link :to="{name:'articleUpdate', params: {ID:article.articleId}}" class="text-decoration-none">
+                  <b-card class="">
+                    <b-card-text class="d-flex justify-content-between">
+                      <h6>제목: {{article.title}}</h6>
+                      <div>
+                        <button class="btn btn-danger btn-sm" @click="deleteArticle({id:article.articleId,temp:0})">삭제하기</button>
+                      </div>
+                    </b-card-text>
+                  </b-card>
+                </router-link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
-      <!-- 3. 하단 -->
-      <div class="container">
-        <div class="row">
-          <div class="col-3">
-              <div class="list-group flex-column" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                  <a class="list-group-item list-group-item-action" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">내가 쓴 게시물</a>
-                  <a class="list-group-item list-group-item-action" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">내가 쓴 후기</a>
-                  <a class="list-group-item list-group-item-action" id="v-pills-3-tab" data-toggle="pill" href="#v-pills-3" role="tab" aria-controls="v-pills-3" aria-selected="false">찜 목록</a>
-                  <a class="list-group-item list-group-item-action" id="v-pills-4-tab" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-4" aria-selected="false">임시 저장</a>
-              </div>
-          </div>
-          <div class="col-9">
-              <div class="tab-content" id="v-pills-tabContent">
-                  <div class="tab-pane fade show active text-dark" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">내가 쓴 게시물 목록
-                    {{ userData.articleList }}</div>
-                  <div class="tab-pane fade text-dark" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-2-tab">내가 쓴 후기 목록</div>
-                  <div class="tab-pane fade text-dark" id="v-pills-3" role="tabpanel" aria-labelledby="v-pills-3-tab">내가 찜한 목록</div>
-                  <div class="tab-pane fade text-dark" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-4-tab">
-                      <div v-for="article in articles" :key="article.articleId">
-                          <!-- <tempList/> -->
-                       <router-link :to="{name:'articleUpdate', params: {ID:article.articleId}}" class="text-decoration-none">
-                          <b-card class="">
-                            <b-card-text class="d-flex justify-content-between">
-                              <h6>제목: {{article.title}}</h6>
-                              <div>
-                          
-                                <button class="btn btn-danger btn-sm" @click="deleteArticle({id:article.articleId,temp:0})">삭제하기</button>
-                              </div>
-                            </b-card-text>
-                          </b-card>
-                          </router-link>
-                      </div>
-                  </div>
-              </div>
-          </div>
-        </div>
     </div>
     <hr>
     <div class="mb-5">
-        <router-link class="btn btn-info ml-1" v-bind:to="{name:constants.URL_TYPE.USER.EDIT}">회원수정</router-link>
-        <button class="btn btn-danger mr-1" @click="deleteUser">회원탈퇴</button>
+      <router-link class="btn btn-info ml-1" v-bind:to="{name:constants.URL_TYPE.USER.EDIT}">회원수정</router-link>
+      <button class="btn btn-danger mr-1" @click="deleteUser">회원탈퇴</button>
     </div>
   </div>
 </template>
@@ -177,6 +159,16 @@ export default {
         border: solid lightgray 1px;
         border-radius: 1%;
         height: 500px;
+    }
+    #v-pills-5 {
+      border: solid lightgray 1px;
+      border-radius: 1%;
+      height: 500px;
+    }
+    #v-pills-6 {
+      border: solid lightgray 1px;
+      border-radius: 1%;
+      height: 500px;
     }
     .profile-photo {
         height: 100px;
